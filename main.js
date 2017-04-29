7da73a1f7a5c4be69ef867948f315fc7c3ccc7a3
Eric Han <scoobaroo415@gmail.com>
2017-04-28 19:29:10 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,11 @@
-//
-//var BTCE = require('btc-e'),
-//    HttpsAgent = require('agentkeepalive').HttpsAgent,
-//    btceTrade = new BTCE("KXB6KT0F-4AJM5O2A-3ZRSF5L1-2X0VNBLS-B9BXAK8N", 
-//        "f1d24fd6adc2ff1f2b8a8b5a03c1a87d34133e4f248a5eab32fb755539c3b696", {
-//      agent: new HttpsAgent()
-//    });
-//  
 var BTCE = require('btc-e'),
 fs = require('fs'),
 currentNonce = fs.existsSync("nonce.json") ? JSON.parse(fs.readFileSync("nonce.json")) : 0,
 // Provide a nonce generation function as the third parameter if desired. 
 // The function must provide a number that is larger than the one before and must not 
 // be larger than the 32-bit unsigned integer max value of 4294967294. 
-btce = new BTCE("KXB6KT0F-4AJM5O2A-3ZRSF5L1-2X0VNBLS-B9BXAK8N", 
-"f1d24fd6adc2ff1f2b8a8b5a03c1a87d34133e4f248a5eab32fb755539c3b696", {
+btce = new BTCE("", 
+"", {
   nonce: function() {
     currentNonce++;
     fs.writeFile("nonce.json", currentNonce);
